b79c6a25e81a8fecedc38a6383438af2e574365d
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2022-02-08 22:43:27 -0800

    added readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+# EMPLOYEE TRACKER 
+
+## Projrct Info 
+ When command line start the app a series of options appear and the user must select one, and based on their selection, they can see departments, roles and employees in the database. 
+
+ ## Languages Used
+ Nodejs, SQL, JS
+
+ 
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,341 @@
-const express = require('express');
-const router = express.Router();
-router.use(require('./departmentRoutes'));
-router.use(require('./employeeRoutes'));
-router.use(require('./roleRoutes'));
+const { express } = require('express');
+const inquirer = require('inquirer');
+const db = require('../../db/connection');
+const cTable = require('console.table');
+
+var departments = [];
+var roles = [];
+var employees = [];
+var updatedRole = [];
 
 
-module.exports = router;
+// enquirer questions to create data
+async function startQuestions() {
+    const question = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'actions',
+            message: "What would you like to do?",
+            choices: ['View all departments', 
+            'View all roles', 
+            'View all employees', 
+            'Add a role', 
+            'Add an employee', 
+            'Add a department', 
+            'Exit']
+        }
+    ])
+
+    // show all departments  
+    if (question.actions === 'View all departments') {
+        viewDepartments();
+       
+
+    }
+
+
+
+    // show all employee roles 
+    if (question.trackerAction === "View all roles") {
+        viewRoles();
+        
+    }
+
+
+    // show all employees 
+    if (question.trackerAction === "View all employees") {
+        viewEmployees();
+       
+    }
+
+
+    // show all roles 
+    let newRole
+    if (question.trackerAction === "Add a role") {
+        newRole = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newRoleTitle',
+                message: "What is the title of the new role? (Required)",
+                validate: newRoleTitle => {
+                    if (newRoleTitle) {
+                        return true;
+                    } else {
+                        console.log("Please enter a role title")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'newRoleSalary',
+                message: "What is the new role's salary? (Required)",
+                validate: newRoleSalary => {
+                    if (newRoleSalary) {
+                        return true;
+                    } else {
+                        console.log("Please enter the role's salary")
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: 'departmentID',
+                message: "What department does the new role belong to?",
+                choices: ['1', '2', '3', '4']
+            }
+
+        ])
+        // add new role to end of array 
+        if (newRole) {
+
+            roles.push(newRole)
+            
+        }
+
+        addRole();
+        
+    }
+
+    // if the user decides to add an employee 
+    if (question.trackerAction === "Add an employee") {
+        
+        newEmployee = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newEmployeeFirstName',
+                message: "What is the employee's first name? (Required)",
+                validate: newEmployeeFirstName => {
+                    if (newEmployeeFirstName) {
+                        return true;
+                    } else {
+                        console.log("Please enter the employee's first name!")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'newEmployeeLastName',
+                message: "What is the employee's last name? (Required)",
+                validate: newEmployeeLastName => {
+                    if (newEmployeeLastName) {
+                        return true;
+                    } else {
+                        console.log("Please enter the employee's last name!")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'roleID',
+                message: "What is the employee's role id? (Required)",
+                validate: roleID => {
+                    if (roleID) {
+                        return true;
+                    } else {
+                        console.log("Please enter the role ID!")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the Manager's name? (Required)",
+                validate: managerName => {
+                    if (managerName) {
+                        return true;
+                    } else {
+                        console.log("Please enter the manager's name!")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'departmentID',
+                message: "What is the employee's department id? (Required)",
+                validate: departmentID => {
+                    if (departmentID) {
+                        return true;
+                    } else {
+                        console.log("Please enter the department ID!")
+                    }
+                }
+            }
+
+        ])
+        if (newEmployee) {
+
+            employees.push(newEmployee);
+            
+        }
+
+        addEmployee();
+       
+    }
+
+        // if the user decides to add a department 
+    if (question.trackerAction === 'Add a department') {
+        newDepartment = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newDepartment',
+                message: "What is the new Department name? (Required)",
+                validate: newDepartment => {
+                    if (newDepartment) {
+                        return true;
+                    } else {
+                        console.log("Please enter a department name!")
+                    }
+                }
+            }
+        ])
+        if (newDepartment) {
+
+            departments.push(newDepartment);
+            
+        }
+        addDepartment();
+       
+    }
+
+
+    // if the user decides to update an employee role
+    
+    // if the user decides to Exit
+    if (question.actions === 'Exit') {
+        return;
+    }
+
+
+
+
+    startQuestions();
+};
+
+
+// show all departments 
+const viewDepartments = () => {
+
+    departments = [];
+
+    db.query(`SELECT * FROM department`, (err, row) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        for (let i = 0; i < row.length; i++) {
+            departments.push(row[i]);
+        }
+        console.table('', departments);
+        
+    })
+
+};
+
+// show all employee roles 
+
+const viewRoles = () => {
+
+    roles = [];
+
+    db.query(`SELECT * FROM employeerole`, (err, row) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        for (let j = 0; j < row.length; j++) {
+            roles.push(row[j]);
+        }
+        console.table('', roles);
+        console.log('Arrow down to perform another action');
+    })
+
+};
+
+// show all employees 
+const viewEmployees = () => {
+    employees = [];
+
+    db.query(`SELECT employee.*, department.department_name AS department, employeerole.title AS role
+    from employee
+    LEFT JOIN department
+    ON employee.department_id = department.id
+    LEFT JOIN employeerole
+    ON employee.role_id = employeerole.id`, (err, row) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        for (let k = 0; k < row.length; k++) {
+            employees.push(row[k]);
+        }
+        
+        console.table('', employees);
+        console.log('Arrow down to perform another action');
+    })
+};
+
+// add a new role 
+const addRole = () => {
+
+
+    const params = [roles[roles.length-1].newRoleTitle, roles[roles.length-1].newRoleSalary, roles[roles.length-1].departmentID];
+
+    db.query(`INSERT INTO employeerole (title, salary, department_id)
+    VALUES (?, ?, ?)`, params, (err, res) => {
+        if (err) {
+
+            return;
+        }
+
+    });
+
+    console.log('The role has been added!')
+    console.log('Arrow down to perform another action');
+
+};
+
+// fadd new emplpyee 
+const addEmployee = () => {
+
+    const params = [employees[employees.length-1].newEmployeeFirstName, 
+    employees[employees.length-1].newEmployeeLastName, 
+    employees[employees.length-1].roleID, 
+    employees[employees.length-1].managerName, 
+    employees[employees.length-1].departmentID];
+    
+    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_name, department_id)
+    VALUES (?, ?, ?, ?, ?)`, params, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+
+        console.log('', "The employee has been added!")
+        console.log("Arrow down to perform another action");
+
+    });
+
+};
+
+
+// add new departments 
+const addDepartment = () => {
+    const params = [departments[departments.length-1].newDepartment];
+    
+    db.query(`INSERT INTO department (department_name)
+    VALUES (?)`, params, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        console.log("The department has been added!")
+        console.log("Arrow down to perform another action");
+    });
+};
+
+
+
+// starts the program 
+startQuestions();
